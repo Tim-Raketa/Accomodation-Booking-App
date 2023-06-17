9cf6aefb1f63f7e43ff0d1e43b5b900edf831015
Dusan-Stanimirovic <93325150+Dusan-Stanimirovic@users.noreply.github.com>
2023-06-18 00:15:18 +0200

    minor changes

---
+++ client-service/src/main/java/com/devProblems/grpc/client/service/NotificationService.java
@@ -0,0 +1,7 @@
+package com.devProblems.grpc.client.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+}
--- notification-service/src/main/java/com/devProblems/grpc/server/model/Notification.java
+++ notification-service/src/main/java/com/devProblems/grpc/server/model/Notification.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class Notification {
+    @Transient
+    public static final String SEQUENCE_NAME = "notification_sequence";
     @Id
-    private Long id;
+    private Integer id;
 
     private String title;
 
